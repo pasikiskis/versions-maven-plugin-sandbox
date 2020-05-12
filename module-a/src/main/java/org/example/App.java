34f930515c04c4f9abb77c4bf2627fd11523b856
Andrius Orlonas <andrius.orlonas@nomagic.com>
2020-05-12 07:41:34 +0300

    init commit

---
+++ module-a/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//import org.example.module_b_package.ModuleBClass;
+
+import org.example.module_b_package.ModuleBClass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ModuleBClass.doSmth();
+    }
+}
---
+++ module-b/src/main/java/org/example/module_b_package/ModuleBClass.java
@@ -0,0 +1,12 @@
+package org.example.module_b_package;
+
+/**
+ * @author Andrius Orlonas
+ */
+public class ModuleBClass
+{
+	public static void doSmth()
+	{
+		System.out.println("ModuleBClass doing smth");
+	}
+}
